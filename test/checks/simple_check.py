0e3d713031059468486f47afa029c534190c5c67
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-05-06 20:29:41 -0700

    Update test

--- test/checks/simple_check.py
+++ test/checks/simple_check.py
@@ -5,6 +5,18 @@ class SimpleTest(unittest.TestCase):
     def test_linking(self):
         self.assertTrue(os.path.islink(expand("~/other/2.txt")))
 
+    def test_no_overwrite(self):
+        self.assertTrue(os.path.isfile(expand("~/test/1.txt")))
+
+    def test_prelink(self):
+        contents = ""
+        with open(expand("~/test/1.txt")) as fin:
+            contents += fin.read().strip()
+        self.assertTrue(contents == "100")
+
+    def test_postlink(self):
+        self.assertTrue(not os.path.exists("~/other/3.txt"))
+
     @classmethod
     def tearDownClass(cls):
         super(SimpleTest, cls).tearDownClass()
