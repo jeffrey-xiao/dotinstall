73078c76a6a562db3f565c2f00251d1a0416fec9
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-08-27 21:12:02 -0400

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'dotinstall',
     packages = ['dotinstall'],
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'Command-line tool to help install and setup your dotfiles.',
     author = 'Jeffrey Xiao',
     author_email = 'jeffrey.xiao1998@gmail.com',
     url = 'https://github.com/jeffrey-xiao/dotinstall',
-    download_url = 'https://github.com/jeffrey-xiao/dotinstall/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/jeffrey-xiao/dotinstall/archive/0.2.0.tar.gz',
     keywords = ['dotfiles', 'commandline'],
     classifiers = [],
     entry_points = {
