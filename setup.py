79135c58f9218666cdaabdc2a7c32a3fd0dcc764
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-08-27 20:47:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from setuptools import find_packages
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name="dotinstall",
-    packages=find_packages(exclude=['tests*']),
+    name = 'dotinstall',
+    packages = ['dotinstall'],
+    version = '0.1.0',
+    description = 'Command-line tool to help install and setup your dotfiles.',
+    author = 'Jeffrey Xiao',
+    author_email = 'jeffrey.xiao1998@gmail.com',
+    url = 'https://github.com/jeffrey-xiao/dotinstall',
+    download_url = 'https://github.com/jeffrey-xiao/dotinstall/archive/0.1.0.tar.gz',
+    keywords = ['dotfiles', 'commandline'],
+    classifiers = [],
 )
