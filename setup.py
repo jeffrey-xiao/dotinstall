19d5f9128bdc9b3913a7608b45836cb53a29be62
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-08-28 23:43:41 -0400

    Bump to 0.4.0

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     install_requires=[
         'PyYAML',
     ],
-    version='0.3.0',
+    version='0.4.0',
     description='Command-line tool to help install and setup your dotfiles.',
     author='Jeffrey Xiao',
     author_email='jeffrey.xiao1998@gmail.com',
     url='https://github.com/jeffrey-xiao/dotinstall',
-    download_url='https://github.com/jeffrey-xiao/dotinstall/archive/0.3.0.tar.gz',
+    download_url='https://github.com/jeffrey-xiao/dotinstall/archive/0.4.0.tar.gz',
     keywords=['dotfiles', 'commandline'],
     classifiers=[],
     entry_points={
